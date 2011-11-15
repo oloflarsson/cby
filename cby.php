523d2bbca4eae16419fc1f91146b679ac9eeaab4
Olof Larsson <olof@sylt.nu>
2011-11-16 00:37:45 +0100

    Doctrine bootstrapig

--- cby.php
+++ cby.php
@@ -8,12 +8,39 @@ Author: Olof Larsson
 Author URI: http://oloflarsson.se
 */
 
-// Bootstrap doctrine
-require 'doctrine/lib/Doctrine/ORM/Tools/Setup.php';
-$lib = 'doctrine'; /// Funkar detta eller måste vi ha abspath?
-Doctrine\ORM\Tools\Setup::registerAutoloadGit($lib);
+define('CBY_ABSPATH', WP_PLUGIN_DIR . '/cby');
 
 class CBY
 {
-	public static $example;
+	// The Doctrine2 EntityManager
+	public static $em;
+	
+	public static function init()
+	{
+		self::doctrine_bootstrap();
+	}
+	
+	public static function doctrine_bootstrap()
+	{
+		// Setup the doctrine class loader
+		require 'doctrine/lib/Doctrine/ORM/Tools/Setup.php';
+		Doctrine\ORM\Tools\Setup::registerAutoloadGit(CBY_ABSPATH . '/doctrine');
+		
+		$paths = array(CBY_ABSPATH . '/entities');
+		$isDevMode = true;
+		$config = Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+		
+		// Database connection information
+		$connectionOptions = array(
+			'driver' =>   'pdo_mysql',
+			'user' =>     DB_USER,
+			'password' => DB_PASSWORD,
+			'host' =>     DB_HOST,
+			'dbname' =>   DB_NAME,
+		);
+		
+		// Create EntityManager
+		self::$em = Doctrine\ORM\EntityManager::create($connectionOptions, $config);
+	}
 }
+CBY::init();
