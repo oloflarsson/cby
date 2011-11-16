04c7e7f1d3add640e8cf5e511469776175af4c35
Olof Larsson <olof@sylt.nu>
2011-11-16 15:33:23 +0100

    Minor fixes to the getlibs.php file

--- getlibs.php
+++ getlibs.php
@@ -11,12 +11,12 @@ set_time_limit(0);
 // NOTE 1: Make sure the revisions are compatible with eachother. You can usualy find that information here: http://www.doctrine-project.org/blog/
 // NOTE 2: Make sure there is a corresponding entry in .gitignore for each of the dependencies.
 $libs = array(
-	array('doctrine-common', 'http://github.com/doctrine/common.git', '2.1.2'),
-	array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', '2.1.3'),
-	array('doctrine-orm', 'http://github.com/doctrine/doctrine2.git', '2.1.2'),
-	array('doctrine-migrations', 'http://github.com/doctrine/migrations.git', 'origin/master'), // does not use tags.
-	array('symfony-console', 'http://github.com/symfony/Console.git', '2.0.5'),
-	array('symfony-yaml', 'http://github.com/symfony/Yaml.git', '2.0.5'),
+	array('doctrine-common', 'git://github.com/doctrine/common.git', '2.1.2'),
+	array('doctrine-dbal', 'git://github.com/doctrine/dbal.git', '2.1.3'),
+	array('doctrine-orm', 'git://github.com/doctrine/doctrine2.git', '2.1.2'),
+	array('doctrine-migrations', 'git://github.com/doctrine/migrations.git', 'origin/master'), // does not use tags.
+	array('symfony-console', 'git://github.com/symfony/Console.git', 'd7b1718424'),
+	array('symfony-yaml', 'git://github.com/symfony/Yaml.git', '6d7a0b450f'),
 );
 
 $libdir = dirname(__FILE__).'/lib';
@@ -34,15 +34,15 @@ foreach ($libs as $lib)
 	if ( ! is_dir($installDir))
 	{
 		$install = true;
-		echo "> Installing $name\n";
+		echo "===== Installing $name =====\n";
 		system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
 	}
 
 	if ( ! $install)
 	{
-        echo "> Updating $name\n";
+        echo "===== Updating $name =====\n";
     }
 
     system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
 }
-echo "> DONE :)\n";
+echo "===== DONE =====\n";
