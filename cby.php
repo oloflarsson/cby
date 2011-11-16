abb416e28b46c294d3357cb64e979f96735fe236
Olof Larsson <olof@sylt.nu>
2011-11-16 20:17:42 +0100

    Fungerande exempel :)

--- cby.php
+++ cby.php
@@ -8,13 +8,27 @@ Author: Olof Larsson
 Author URI: http://oloflarsson.se
 */
 
+use Entities\Person;
+
 require_once 'bootstrap.php';
 
 class CBY
 {
 	public static function init()
 	{
-		// TODO...
+		global $em;
+		
+		$person = new Person;
+		$person->setFirstname('Sven');
+		$person->setNick('Svempa');
+		$person->setLastname('Svensson');
+		$person->setSsn(1212121212);
+		$person->setPhone('070121212');
+		$person->setComment('Wooop opoop po poop po  o popo');
+		$person->setAllergycomment('jag er elergisk mot hest');
+		
+		$em->persist($person);
+		$em->flush();
 	}
 }
 CBY::init();
