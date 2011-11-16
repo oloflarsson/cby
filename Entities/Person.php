063317dd72504c0fc1acf1299ec1c185914e9fdc
Olof Larsson <olof@sylt.nu>
2011-11-16 20:00:25 +0100

    Opps. Lost the gitignore.

--- Entities/Person.php
+++ Entities/Person.php
@@ -37,15 +37,25 @@ class Person
 	public function getPhone() { return $this->phone; }
 	public function setPhone($val) { $this->phone = $val; }
 	
-	// XS, S, M, L, XL, XXL
-	/** @ManyToOne(targetEntity="TShirtType") */
-    private $tshirttype;
-	public function getTShirtType() { return $this->tshirttype; }
-	public function setTShirtType(TShirtType $val) { $this->tshirttype = $val; }
+	/** @Column(type="string", length=255) */
+	private $comment;
+	public function getComment() { return $this->comment; }
+	public function setComment($val) { $this->comment = $val; }
+	
+	/** @Column(type="string", length=255) */
+	private $allergycomment;
+	public function getAllergycomment() { return $this->allergycomment; }
+	public function setAllergycomment($val) { $this->allergycomment = $val; }
 	
 	// Omnivore, Vegetarian, Vegan
 	/** @ManyToOne(targetEntity="ConsumerType") */
     private $consumertype;
 	public function getConsumerType() { return $this->consumertype; }
 	public function setConsumerType(ConsumerType $val) { $this->consumertype = $val; }
+	
+	// XS, S, M, L, XL, XXL
+	/** @ManyToOne(targetEntity="TShirtType") */
+    private $tshirttype;
+	public function getTShirtType() { return $this->tshirttype; }
+	public function setTShirtType(TShirtType $val) { $this->tshirttype = $val; }
 }
